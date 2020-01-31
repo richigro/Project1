17ea3ec036557fa08b5dd67052bf682c7629e65a
Ricardo Guerrero <richi_gro@hotmail.com>
2020-01-31 13:14:50 -0600

    added a ne config file to connect to local mongodb server for testing not production

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 'use strict';
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {Plane} = require('./modles');
 
 app.get('/', (req, res) => {
     res.send('Its alive');
